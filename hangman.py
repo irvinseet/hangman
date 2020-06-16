2ae602313d308a441f365311b0b1c1e6e4f128dd
irvinseet <61399162+irvinseet@users.noreply.github.com>
2020-06-16 16:41:55 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -11,54 +11,55 @@ l1 = []
 
 #read file
 with open('words.txt', 'r') as f:
-  l = f.readlines()
+  fhand = f.readlines()
   f.close()
 
 #remove /n after every element
-l1 = [element.strip().upper() for element in l]
+l1 = [element.strip().upper() for element in fhand]
 
 #select a random word from the dictionary
 word = (random.choice(l1))
 
 #print display for user
 print('Welcome to Hangman!')
-print(str(len(word)) + ' letters')
+print('{} letters'.format(len(word)))
 
 #use a list for easier manipulation
-guesslist = [] 
+guesslist = list()
 for i in range(len(word)):
   guesslist.append('_')
 
 #converts guesslist into a string for better display
+#printing the above guesslist will return ['_', '_' ... etc]
+#and we dont want that
 guessprint = (' '.join(guesslist))
 print(guessprint)
 
 #initialise counter
-errorcounter = 0
+lives = 6
 
 while True:
-  print('You have ' + str(int(6 - errorcounter)) + ' lives')
+  print('You have {} lives left'.format(lives))
   letter = input("Guess a letter: ").upper()
 
   if letter == 'EXIT': #user can type 'exit' to stop playing
     sys.exit()
 
   if len(letter) != 1 or letter.isalpha() == False:
-    print("Error: Type an aphabetical letter.")
+    print("Error: Please guess an aphabetical letter.")
     
 #iterate through word to see if letter matches
   for i in range(len(word)):
     if letter == word[i]:
       guesslist[i] = letter #update the list
-      errorcounter -= 1 # to neutralise the line below
-      
-  errorcounter += 1
+      guessprint = (' '.join(guesslist)) # print the updated list
+      print(guessprint)
+      lives = lives + 1 #to neutralise the subtraction outside the loop aft every round
 
-  guessprint = (' '.join(guesslist)) # print the updated list
-  print(guessprint)
+  lives = lives - 1 #outside the for loop so it doesnt iterate len(word) times
 
 #user fail to guess word with 6 lives
-  if errorcounter == 6:
+  if lives == 0:
     print("Game over! The word is " + word)
     sys.exit()
 
